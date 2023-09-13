300e2572a92a1ed75c40eee0645133db2705c283
jinjiaxin77 <1665353392@qq.com>
2023-09-13 16:37:15 +0800

    add test

---
+++ src/test/java/com/jinjiaxin/yixiapan/YixiaPanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinjiaxin.yixiapan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YixiaPanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
