122b63581d87ff10f333a562321c70ffd329d491
jinjiaxin77 <1665353392@qq.com>
2023-09-13 16:37:15 +0800

    add test

---
+++ src/main/java/com/jinjiaxin/yixiapan/YixiaPanApplication.java
@@ -0,0 +1,13 @@
+package com.jinjiaxin.yixiapan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YixiaPanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YixiaPanApplication.class, args);
+    }
+
+}
